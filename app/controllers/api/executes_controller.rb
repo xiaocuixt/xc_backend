120044cc4ffbb2454cf132ec02609721854b5ece
xiaocuixt <cjw624923@126.com>
2017-08-24 17:47:34 +0800

    配置graphql

---
+++ app/controllers/api/executes_controller.rb
@@ -0,0 +1,4 @@
+class Api::ExecutesController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  post "/graphql", to: "graphql#execute"
+  resources :posts
+  namespace :api do
+    resource :excutes, only: :index, via: [:post, :options]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/api/excutes"
+  end
 end
